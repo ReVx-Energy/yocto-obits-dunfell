752ca6214f90e2de0c8963a6d2262bb9c4c2e645
Arnob Chanda <arnob.chanda@aesi-ess.com>
2025-07-22 18:38:50 +0000

    Updating devcontainer, gitignore, vscode settings, manifest and the setup script

--- setup_system.sh
+++ setup_system.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Clone all the repositories
 repo init -u $PWD -m manifest.xml
 repo sync -j$(nproc --all)
+
+TEMPLATECONF=$PWD/meta-renesas/meta-rzg2h/docs/template/conf/ source sources/poky/oe-init-build-env build
